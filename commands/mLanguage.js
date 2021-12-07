3a43abe48e49a597eb9d0761f2fdaad016ec6e84
SyedMuradAliShah <55234974+SyedMuradAliShah@users.noreply.github.com>
2021-12-07 15:33:52 +0500

    Language fixed

--- commands/mLanguage.js
+++ commands/mLanguage.js
@@ -1,48 +1,34 @@
 var capitalize = require('./functions');
-const fsa = require("fs");
 module.exports = function (vscode, fs, path, pathdir) {
     vscode.window.showInputBox({
-        prompt: "Enter language file name",
-        placeHolder: "Enter language file name"
-    }).then(function (lang) {
-        if (lang.length == 0) {
-            vscode.window.showErrorMessage("You should insert language name.");
+        prompt: "Enter name of language file",
+        placeHolder: "Enter name of language file"
+    }).then(function (val) {
+        if (val.length == 0) {
+            vscode.window.showErrorMessage("Language file name required.");
+
         } else {
-            var pathlang = path.join(pathdir + "/application/language", lang);
-            fs.access(pathlang, function (err) {
-                if (err) fsa.mkdirSync(pathlang);
-                    vscode.window.showInputBox({
-                        prompt: "file name",
-                        placeHolder: "set file name"
-                    }).then(function (val) {
-                        if (val.length == 0) {
-                            vscode.window.showErrorMessage("Language file name required.");
-                        } else {
-                            var pathfile = path.join(pathdir + "/application/language", lang + '/' + capitalize.capitalize(val) + "_lang") + ".php";
-                            fs.access(pathfile, function (err) {
-                                if (!err) {
-                                    vscode.window.showWarningMessage("Language file name already exists  !");
-                                } else {
-                                    fs.open(pathfile, "w+", function (err, fd) {
-                                        if (err) throw err;
-                                        fs.writeFileSync(fd, `<?php 
+            var pathfile = path.join(pathdir + "/application/language", `${capitalize.lowercase(val)}_lang.php`);
+            fs.access(pathfile, function (err) {
+                if (!err) {
+                    vscode.window.showWarningMessage("Language file name already exists!");
+                } else {
+                    fs.open(pathfile, "w+", function (err, fd) {
+                        if (err) throw err;
+                        fs.writeFileSync(fd, `<?php 
+
 $lang['helloWorld'] = 'hello world'; 
 
-
-/* End of file ${capitalize.capitalize(val)}.php and path  ${pathfile.replace(pathdir,'')} */
-
+/* End of file ${capitalize.lowercase(val)}_lang.php and path ${pathfile.replace(pathdir,'')} */
 `);
-                                    fs.close(fd);
-                                });
-                                var openPath = vscode.Uri.file(pathfile); //A request file path
-                                vscode.workspace.openTextDocument(openPath).then(function (val) {
-                                    vscode.window.showTextDocument(val);
-                                });
-                                vscode.window.showInformationMessage('Language created successfully! ');
-                            }
+                        fs.close(fd);
+                        var openPath = vscode.Uri.file(pathfile); //A request file path
+                        vscode.workspace.openTextDocument(openPath).then(function (val) {
+                            vscode.window.showTextDocument(val);
                         });
-                    }
-                });
+                    });
+                    vscode.window.showInformationMessage('Language file created successfully! ');
+                }
             });
         }
     });
